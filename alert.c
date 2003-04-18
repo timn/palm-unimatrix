cef9439aa106f8ac1d6aefbd379e75e3f287ecb9
Tim Niemueller <tim@niemueller.de>
2003-04-18 23:54:52 +0000

    Removing alert files. Should be alarm and not alert... Not an emergency. Hmm, exams, maybe it IS an alert? :-)

--- alert.c
+++
@@ -1,6 +0,0 @@
-/* $Id: alert.c,v 1.1 2003/04/18 23:51:45 tim Exp $
- *
- * Support for exam alerts
- * Created: 2003/04/19
- * Portions copyright (c) 2000 Palm, Inc. or its subsidiaries.  All rights reserved.
- */
