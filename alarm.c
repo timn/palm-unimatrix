4c2befe1f73177337d6b280efdd12f99ebb610b7
Tim Niemueller <tim@niemueller.de>
2003-04-18 23:56:26 +0000

    Adding alarm support files.

---
+++ alarm.c
@@ -0,0 +1,6 @@
+/* $Id: alarm.c,v 1.1 2003/04/18 23:56:26 tim Exp $
+ *
+ * Support for exam alarms
+ * Created: 2003/04/19
+ * Portions copyright (c) 2000 Palm, Inc. or its subsidiaries.  All rights reserved.
+ */
---
+++ alarm.h
@@ -0,0 +1,13 @@
+/* $Id: alarm.h,v 1.1 2003/04/18 23:56:26 tim Exp $
+ *
+ * Support for exam alarms
+ * Created: 2003/04/19
+ * Portions copyright (c) 2000 Palm, Inc. or its subsidiaries.  All rights reserved.
+ */
+
+#ifndef ALARM_H
+#define ALARM_H
+
+#include "UniMatrix.h"
+
+#endif /* ALARM_H */
