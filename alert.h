4220569cbaeab3818223d12abe4190ad72f34f5d
Tim Niemueller <tim@niemueller.de>
2003-04-18 23:51:45 +0000

    Added plain template files for alert support.

---
+++ alert.h
@@ -0,0 +1,13 @@
+/* $Id: alert.h,v 1.1 2003/04/18 23:51:45 tim Exp $
+ *
+ * Support for exam alerts
+ * Created: 2003/04/19
+ * Portions copyright (c) 2000 Palm, Inc. or its subsidiaries.  All rights reserved.
+ */
+
+#ifndef _ALERT_H
+#define _ALERT_H
+
+#include "UniMatrix.h"
+
+#endif /* _ALERT_H */
