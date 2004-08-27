edbad34b9b562efd0d46daba058356e12b3962ea
Tim Niemueller <tim@niemueller.de>
2004-08-27 20:39:58 +0000

    Webpage is no longer part of the repository to make maintenance easier. This file contains the list of supported languages now which makes sense here.
    Tim

---
+++ webpage/languages.inc.php
@@ -0,0 +1,25 @@
+<? php
+
+// $Id: languages.inc.php,v 1.1 2004/08/27 20:39:58 tim Exp $
+
+$languages=array( "de" => "German/Deutsch",
+                  "en" => "English",
+                  "pt" => "Portugues",
+                  "fr" => "French/Francais",
+                  "gr" => "Greek",
+                  "es" => "Spanish",
+                  "si_latin1" => "Slovenian (Latin1)",
+                  "si_latin2" => "Slovenian (Latin2)",
+                  "si_cp1250" => "Slovenian (cp1250)",
+                  "nl" => "Dutch",
+                  "pl_iso8859-1" => "Polish (ISO 8859-1)",
+                  "pl_iso8859-2" => "Polish (ISO 8859-2)",
+                  "cz_iso8859-2" => "Czech (ISO 8859-2)",
+                  "cz_cp1250" => "Czech (cp1250)",
+                  "it" => "Italian",
+                  "ca" => "Catalan",
+                  "kr" => "Korean",
+                  "il" => "Hebrew (Israel)",
+                );
+
+?>
